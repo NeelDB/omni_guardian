1f2534d368e20c1233cecb5d6e1d88acf2204643
NeelDB <n.badracim@campus.fct.unl.pt>
2024-05-11 19:23:27 +0100

    Inicio da galeria e ip's atualizados

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:omni_guardian/components/my_app_bar.dart';
 
-class Gallery extends StatelessWidget {
+class Gallery extends StatefulWidget {
   const Gallery({super.key});
 
   @override
+  State<Gallery> createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+
+  List<Map<String, String>> images = [
+    {
+      'img': 'https://example.com/image1.jpg',
+      'caption': 'Caption for Image 1',
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Gallery",
-        style: TextStyle(fontSize: 50),
+    return Scaffold(
+      appBar: MyAppBar('Gallery'),
+      body: images.isEmpty? const Center(
+        child: Text(
+          'No images yet',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ) : SingleChildScrollView(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 5.0,
+              ),
+              itemCount: images.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    // You can add onTap functionality here, like showing full-size image
+                    // or navigating to another page.
+                  },
+                  child: Image.network(
+                    images[index][1]!,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+
+        ),
       ),
     );
   }
-
 }
--- lib/rest/requests.dart
+++ lib/rest/requests.dart
@@ -10,12 +10,12 @@ import '../data/User.dart';
 class Requests {
 
   // Home Server and Camera - Bessa
-  //static const String _serverHost = '192.168.1.74';
-  //static const String _cameraHost = '192.168.1.147';
+  static const String _serverHost = '192.168.113.237';
+  static const String _cameraHost = '192.168.113.196';
 
   // Home Server and Camera - Neel
-  static const String _serverHost = '192.168.1.5';
-  static const String _cameraHost = '192.168.1.147';
+  //static const String _serverHost = '192.168.1.5';
+  //static const String _cameraHost = '192.168.1.147';
 
   // Remote Server and Camera - Bessa
   //static const String _serverHost = '192.168.105.237';
